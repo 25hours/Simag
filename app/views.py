0c07f7334b4952740d6c2d1bc06790041efcd257
25hours <shawnniu@yeah.net>
2017-04-06 09:25:42 +0800

    testing login page

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template,request,url_for,redirect
+from app.forms import LoginForm
+from app.models import *
 
 @app.route('/')
 def index():
@@ -8,6 +10,15 @@ def index():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/proto_login',methods=['GET','POST'])
+def proto_login():
+    form = LoginForm()
+    if request.method == 'POST':
+        # infos = User.query.filter_by(username=username)
+        if request.form['username'] == 'test1' and request.form['password'] == '123':
+            return redirect(url_for('proto_publish'))
+    return render_template('proto_login.html',form=form)
 
 @app.route('/publish')
 def publish():
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root123@192.168.11.137:3306/Simag'
 SQLALCHEMY_TRACK_MODIFICATIONS = 'True'
+SECRET_KEY = '\xc7pxW5\xc2\xfc\xfa\xa6\x17&SG<\x894\xfb\x97\xd2ZYS\xf7'
