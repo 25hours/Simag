96bd8dd4265530568f70ac5475cf40aac4b8f087
25hours <shawnniu@yeah.net>
2017-04-30 15:16:50 +0800

    add init_project and init_code_server in manage.py

--- manage.py
+++ manage.py
@@ -22,5 +22,24 @@ def init_task():
     db.session.add(test_tasks)
     db.session.commit()
 
+@manager.command
+def init_project():
+    test_project1 = Project(project='project1')
+    test_project2 = Project(project='project2')
+    test_project3 = Project(project='project3')
+    test_project4 = Project(project='project4')
+    test_project5 = Project(project='project5')
+    db.session.add_all([test_project1,test_project2,test_project3,test_project4,test_project5])
+    db.session.commit()
+
+@manager.command
+def init_code_server():
+    code_server1 = Code_Server(code_server='192.168.1.1')
+    code_server2 = Code_Server(code_server='192.168.1.2')
+    code_server3 = Code_Server(code_server='192.168.1.3')
+    code_server4 = Code_Server(code_server='192.168.1.4')
+    db.session.add_all([code_server1,code_server2,code_server3,code_server4])
+    db.session.commit()
+
 if __name__ == '__main__':
     manager.run()
